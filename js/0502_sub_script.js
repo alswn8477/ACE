82acfb8549e1267c1dad4adcd2f56d66143d0d50
alswn8477 <alswn8477@naver.com>
2025-05-02 23:22:38 +0900

    first commit

--- js/0502_sub_script.js
+++ js/0502_sub_script.js
@@ -1,4 +1,5 @@
 // header
+let header = document.querySelector("header");
 let nav = document.querySelector(".nav");
 let gnbL = document.querySelector(".left");
 let sub = document.querySelector(".left").querySelectorAll(".two_dep");
@@ -12,14 +13,17 @@ gnbL.onmouseenter = function () {
     spans[0].style.display = "none"; // 첫 번째 span 숨기고
     spans[1].style.display = "block"; // 두 번째 span 보이기
   });
-  document.querySelector(".nav").style.height = "280px";
+  // document.querySelector(".nav").style.height = "280px";
+  // document.querySelector(".nav").style.background = "#fff";
+  header.style.height = "330px";
+  header.style.background = "#fff";
 
   sub.forEach(function (v, k) {
     v.classList.add("on");
   });
   nav.classList.add("on");
   document.querySelector(".left").classList.add("on");
-  this.querySelector(".down").style.color = "#0890c0";
+  // this.querySelector(".down").style.color = "#0890c0";
   this.classList.add("on");
 };
 gnbL.onmouseleave = function () {
@@ -29,7 +33,10 @@ gnbL.onmouseleave = function () {
     spans[0].style.display = "block"; // 첫 번째 span 숨기고
     spans[1].style.display = "none"; // 두 번째 span 보이기
   });
-  document.querySelector(".nav").style.height = "0";
+  // document.querySelector(".nav").style.height = "0";
+  header.style.height = "330px";
+  header.style.background = "transparent";
+
   sub.forEach(function (v, k) {
     v.classList.remove("on");
   });
@@ -41,21 +48,28 @@ gnbL.onmouseleave = function () {
 gnbR.onmouseenter = function () {
   document.querySelector(".logo").style.opacity = "0";
   document.querySelector(".nav").style.height = "280px";
+  // header.style.background = "#fff";
+  header.style.height = "330px";
+  header.style.background = "#fff";
+
   sub2.forEach(function (v, k) {
     v.classList.add("on");
   });
   nav.classList.add("on");
   document.querySelector(".right").classList.add("on");
-  v.querySelector("span").style.color = "#0890c0";
+  // sub2.querySelector("span").style.color = "#0890c0";
 };
 gnbR.onmouseleave = function () {
   document.querySelector(".logo").style.opacity = "1";
   document.querySelector(".nav").style.height = "0";
+  header.style.height = "330px";
+  header.style.background = "transparent";
+
   sub2.forEach(function (v, k) {
     v.classList.remove("on");
   });
   document.querySelector(".right").classList.remove("on");
-  v.querySelector("span").style.color = "#333";
+  // sub2.querySelector("span").style.color = "#333";
 };
 
 let prevScroll = window.pageYOffset;
